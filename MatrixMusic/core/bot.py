342698eddd00379d903d6be517328125f6285787
abbasghazal <134511359+abbasghazal@users.noreply.github.com>
2025-09-21 09:43:31 +0300

    Add files via upload

---
+++ MatrixMusic/core/bot.py
@@ -0,0 +1,43 @@
+from pyrogram import Client, errors
+from pyrogram.enums import ChatMemberStatus, ParseMode
+import config
+from ..logging import LOGGER
+
+class Zelzaly(Client):
+    def __init__(self):
+        LOGGER("ميــوزك بحر").info(f"جارِ بدء تشغيل البوت . . .")
+        super().__init__(
+            name="YousefMusic",
+            api_id=config.API_ID,
+            api_hash=config.API_HASH,
+            bot_token=config.BOT_TOKEN,
+        )
+    
+    async def start(self):
+        await super().start()
+        self.id = self.me.id
+        self.name = self.me.first_name + " " + (self.me.last_name or "")
+        self.username = self.me.username
+        self.mention = self.me.mention
+        
+        try:
+            await self.send_photo(
+                chat_id=config.LOGGER_USERNAME,  # استخدام اليوزرنيم بدلاً من ID
+                photo="https://forkgraph.zaid.pro/file/CyY9CNwrPaNW",
+                caption=f"<b> {self.mention}\n تم تشغيل البـوت :\n على سورس بحر :\nɴᴀᴍᴇ : {self.name}\nᴜꜱᴇʀ ɴᴀᴍᴇ : @{self.username}\nɪᴅ : {self.id}</b>",
+            )
+        except (errors.ChannelInvalid, errors.UsernameInvalid, errors.UsernameNotOccupied):
+            LOGGER(__name__).error(
+                "» قم باضافة البـوت مشـرفـاً بكافة الصلاحيات في مجموعـة السجـل"
+            )
+            exit()
+        except Exception as ex:
+            LOGGER(__name__).error(
+                f"Bot has failed to access the log group/channel.\n  Reason : {type(ex).__name__}."
+            )
+            exit()
+        
+        LOGGER("ميــوزك بحر").info(f" تم بدء تشغيل البوت {self.name} ...✓")
+    
+    async def stop(self):
+        await super().stop()
